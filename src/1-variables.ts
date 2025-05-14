30227a437388f4992e0a53603250430bfe2bf7be
kimaiyo <meshackkimaiyo5@gmail.com>
2025-05-14 11:26:52 +0300

    Add initial TypeScript project setup with configuration files and example code

---
+++ src/1-variables.ts
@@ -0,0 +1,5 @@
+const variables = () =>{
+    console.log("hello this is variables")
+}
+
+export default variables
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log("hello ts")
+import variables from "./1-variables"
+
+variables()
